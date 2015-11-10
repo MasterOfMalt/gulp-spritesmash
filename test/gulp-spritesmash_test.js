c4184b031462823255a5636667d2fde12bd5aac1
Alex McAuliffe <alex@masterofmalt.com>
2015-11-10 16:30:38 +0000

    Generated tests for less, scss and css

--- test/gulp-spritesmash_test.js
+++ test/gulp-spritesmash_test.js
@@ -8,54 +8,70 @@ before(function (done) {
   rimraf(__dirname + '/actual-files/', done);
 });
 
+var names = [
+	'default',
+	'scss',
+	'less'
+]
+
 describe('gulp-spritesmash', function () {
-	describe('running a task without any options', function() {
-		childUtils.run('gulp sprite-default-A');
-		
-		it('generates an image', function () {
-			assert.doesNotThrow(function() {
-				var files = fs.readdirSync(__dirname + '/actual-files/default/A/')				
-				var name = _.find(files, function(item) { 
-					return item.match(/sprite-.*/g)
+
+	names.forEach(function(name) {
+		['A', 'B'].forEach(function(variation) {
+			describe(`running a task without any options - ${name} : ${variation}`, function() {
+				childUtils.run(`gulp sprite-${name}-${variation}`);
+				
+				it('generates an image', function () {
+					assert.doesNotThrow(function() {
+						var files = fs.readdirSync(__dirname + `/actual-files/${name}/${variation}/`)				
+						var spriteFile = _.find(files, function(item) { 
+							return item.match(/sprite-.*/g)
+						});
+						
+						fs.readFileSync(__dirname + `/actual-files/${name}/${variation}/${spriteFile}`)
+					})
 				});
 				
-				fs.readFileSync(__dirname + '/actual-files/default/A/' + name)
-			})
-		});
-		
-		it('generates a css file', function () {
-			assert.doesNotThrow(function() {
-				fs.readFileSync(__dirname + '/actual-files/default/A/spriteCss.css')
+				it('generates a css file', function () {
+					assert.doesNotThrow(function() {
+						var actualFiles = fs.readdirSync(__dirname + `/actual-files/${name}/${variation}/`)				
+						var actualCssFile = _.find(actualFiles, function(item) { 
+							return item.match(/spriteCss\..*/g)
+						});
+						
+						var expectedFiles = fs.readdirSync(__dirname + `/actual-files/${name}/${variation}/`)				
+						var expectedCssFile = _.find(expectedFiles, function(item) { 
+							return item.match(/spriteCss\..*/g)
+						});
+						
+						var actualCss = fs.readFileSync(__dirname + `/actual-files/${name}/${variation}/${actualCssFile}`, 'utf8');
+						var expectedCss = fs.readFileSync(__dirname + `/expected-files/${name}/${variation}/${expectedCssFile}`, 'utf8');
+						assert.strictEqual(actualCss, expectedCss);				
+					})
+				});
 				
-				var actualCss = fs.readFileSync(__dirname + '/actual-files/default/A/spriteCss.css', 'utf8');
-     			var expectedCss = fs.readFileSync(__dirname + '/expected-files/default/A/spriteCss.css', 'utf8');
-				assert.strictEqual(actualCss, expectedCss);				
-			})
-		});		
-	});
+			});
+		})
+	}, this)
 	
-	describe('running a task without any options - scss', function() {
-		childUtils.run('gulp sprite-scss-A');
+	describe('running any task', function() {
+		childUtils.run('gulp sprite-default-A');
+		childUtils.run('gulp sprite-default-B');
 		
-		it('generates an image', function () {
+		it('generates a different image for different content', function () {
 			assert.doesNotThrow(function() {
-				var files = fs.readdirSync(__dirname + '/actual-files/scss/A/')				
-				var name = _.find(files, function(item) { 
+				var filesA = fs.readdirSync(__dirname + '/actual-files/default/A/')				
+				var nameA = _.find(filesA, function(item) { 
 					return item.match(/sprite-.*/g)
 				});
 				
-				fs.readFileSync(__dirname + '/actual-files/scss/A/' + name)
-			})
-		});
-		
-		it('generates a css file', function () {
-			assert.doesNotThrow(function() {
-				fs.readFileSync(__dirname + '/actual-files/scss/A/spriteCss.scss')
+				var filesB = fs.readdirSync(__dirname + '/actual-files/default/B/')				
+				var nameB = _.find(filesB, function(item) { 
+					return item.match(/sprite-.*/g)
+				});
 				
-				var actualCss = fs.readFileSync(__dirname + '/actual-files/scss/A/spriteCss.scss', 'utf8');
-     			var expectedCss = fs.readFileSync(__dirname + '/expected-files/scss/A/spriteCss.scss', 'utf8');
-				assert.strictEqual(actualCss, expectedCss);				
+				assert.notEqual(nameA, nameB);
 			})
-		});		
+		});	
 	});
 });
