716560e43d1655d5e4b01a5cc267b0b27e9401d7
Alex McAuliffe <alex@masterofmalt.com>
2015-11-10 14:59:39 +0000

    Add simple gulp test

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
+var spritesmash = require('../src/index.js');
 
-gulp.task('default', function() {
-	console.log('Hello World')
+// Define our test tasks
+var images = [
+  'test-files/sprite1.png',
+  'test-files/sprite2.png',
+  'test-files/sprite3.png',
+  'test-files/sprite4.png',
+  'test-files/sprite5.png'
+];
+
+gulp.task('sprite-default', function () {
+  return gulp.src(images)
+    .pipe(spritesmith({
+      imgName: './folder/sprite.png',
+      cssName: 'sprite.css'
+    }))
+    .pipe(spritesmash())
+  	.pipe(gulp.dest('actual-files/default/'));
 });
