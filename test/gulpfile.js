660bd96ff169d2e28d2c123550c28503ae4eeae4
Alex McAuliffe <alex@masterofmalt.com>
2015-11-10 15:13:57 +0000

    Add variations on test with a changed file

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -3,7 +3,7 @@ var spritesmith = require('gulp.spritesmith');
 var spritesmash = require('../src/index.js');
 
 // Define our test tasks
-var images = [
+var imagesA = [
   'test-files/sprite1.png',
   'test-files/sprite2.png',
   'test-files/sprite3.png',
@@ -11,12 +11,72 @@ var images = [
   'test-files/sprite5.png'
 ];
 
-gulp.task('sprite-default', function () {
-  return gulp.src(images)
+// Define our test tasks
+var imagesB = [
+  'test-files/sprite1.png',
+  'test-files/sprite2.png',
+  'test-files/sprite3.png',
+  'test-files/sprite4-diff.png',
+  'test-files/sprite5.png'
+];
+
+gulp.task('sprite-default-A', function () {
+  return gulp.src(imagesA)
     .pipe(spritesmith({
       imgName: './folder/sprite.png',
       cssName: 'sprite.css'
     }))
     .pipe(spritesmash())
-  	.pipe(gulp.dest('actual-files/default/'));
+  	.pipe(gulp.dest('actual-files/default/A/'));
+});
+
+gulp.task('sprite-default-B', function () {
+  return gulp.src(imagesB)
+    .pipe(spritesmith({
+      imgName: './folder/sprite.png',
+      cssName: 'sprite.css'
+    }))
+    .pipe(spritesmash())
+  	.pipe(gulp.dest('actual-files/default/B/'));
+});
+
+
+gulp.task('sprite-scss-A', function () {
+  return gulp.src(imagesA)
+    .pipe(spritesmith({
+      imgName: './folder/sprite.png',
+      cssName: 'sprite.scss'
+    }))
+    .pipe(spritesmash())
+  	.pipe(gulp.dest('actual-files/scss/A/'));
+});
+
+gulp.task('sprite-scss-B', function () {
+  return gulp.src(imagesB)
+    .pipe(spritesmith({
+      imgName: './folder/sprite.png',
+      cssName: 'sprite.scss'
+    }))
+    .pipe(spritesmash())
+  	.pipe(gulp.dest('actual-files/scss/B/'));
+});
+
+gulp.task('sprite-less-A', function () {
+  return gulp.src(imagesA)
+    .pipe(spritesmith({
+      imgName: './folder/sprite.png',
+      cssName: 'sprite.less'
+    }))
+    .pipe(spritesmash())
+  	.pipe(gulp.dest('actual-files/less/A/'));
+});
+
+gulp.task('sprite-less-B', function () {
+  return gulp.src(imagesB)
+    .pipe(spritesmith({
+      imgName: './folder/sprite.png',
+      cssName: 'sprite.less'
+    }))
+    .pipe(spritesmash())
+  	.pipe(gulp.dest('actual-files/less/B/'));
 });
