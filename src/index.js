046ef7f966dcbc80d6ade0db0cda25276d614632
Alex McAuliffe <alex@masterofmalt.com>
2015-11-11 11:27:40 +0000

    Added options to add types and Hash Functions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ var _ = require('lodash');
 var path = require('path');
 var gutil = require('gulp-util');
 
-function spriteSmash(params) {
+function spriteSmash(options) {
+	
+	var opts = options || {};
+	opts.updateFormats = opts.updateFormats || [];
+	opts.revisionFormats = opts.revisionFormats || [];
+	opts.hashFunction = opts.hashFunction || 'MD5';
 	
 	var renames = [];
 	var revisionFormats = [
@@ -13,7 +18,7 @@ function spriteSmash(params) {
 		'jpg',
 		'svg',
 		'gif'
-	]
+	].concat(opts.revisionFormats)
 	
 	var updateFormats = [
 		'styl',
@@ -21,9 +26,14 @@ function spriteSmash(params) {
 		'sass',
 		'scss',
 		'less',
-		'css',
-		'md'
-	]
+		'css'
+	].concat(opts.updateFormats)
+	
+	var HashFunctions = {
+		'MD5': MD5Hash,
+		'Timestamp': timestampHash,
+		'SHA1': SHA1Hash
+	}
 	
 	var allExtensions = revisionFormats.concat(updateFormats);
 	
@@ -45,8 +55,10 @@ function spriteSmash(params) {
 		// Collect renames from reved files.
 		if (file.revOrigPath) {
 			renames.push({
-				originalName: path.normalize(path.relative(file.revOrigBase, file.revOrigPath)),
-				newName: path.normalize(path.relative(file.base, file.path)),
+				originalName: file.revOrigBase,
+				originalPath: path.normalize(path.relative(file.revOrigBase, file.revOrigPath)),
+				newName: file.base,
+				newPath: path.normalize(path.relative(file.base, file.path))
 			});
 			skip = !_.includes(updateFormats, fileExt.slice(1, fileExt.length));
 		}
@@ -73,20 +85,27 @@ function spriteSmash(params) {
 			return _.includes(updateFormats, ext.slice(1, ext.length));
 		})
 		
-		revisionFiles.forEach(function(file) {
-			var hash = crypto.createHash('md5').update(file.contents).digest('hex').slice(0, 10);
-			var filePath = path.parse(file.path)
-			filePath.base = `${filePath.name}-${hash}${filePath.ext}`
-			var newName = path.normalize(path.format(filePath))
-			var originalName = file.path;
+		revisionFiles.forEach(function(file) {			
+			var hashFunc = _.isFunction(opts.hashFunction) ? 
+							opts.hashFunction :
+							HashFunctions[opts.hashFunction];
 			
+			var filePath = path.parse(file.path)
+			var newName = hashFunc(filePath, file.contents);
+			var originalName = filePath.base;
+			filePath.base = newName;
+			var newPath = path.normalize(path.format(filePath)).split(/[?#]/)[0];
+			var originalPath = file.path.split(/[?#]/)[0];
+						
 			renames.push({
 				newName: newName,
-				originalName: file.path					
+				newPath: newPath,
+				originalName: originalName,
+				originalPath: originalPath	
 			})
 			
 			file = _.merge(file, {
-				path: newName,
+				path: newPath,
 				originalName: originalName
 			});
 			
@@ -96,7 +115,11 @@ function spriteSmash(params) {
 		updateFiles.forEach(function(file) {
 			var contents = file.contents.toString();
 			renames.forEach(function replace(renamed) {
-				contents = contents.split(renamed.originalName).join(renamed.newName);
+				contents = contents
+							.split(renamed.originalPath)
+							.join(renamed.newPath)
+							.split(renamed.originalName)
+							.join(renamed.newName);
 			});
 			file.contents = new Buffer(contents);
 			that.push(file);
@@ -109,5 +132,23 @@ function spriteSmash(params) {
 	return retStream;
 }
 
+function buildHashString(filePath, hash) {
+	return `${filePath.name}-${hash}${filePath.ext}`	
+}
+
+function MD5Hash(filePath, contents) {
+	var hash = crypto.createHash('md5').update(contents).digest('hex').slice(0, 10);
+	return buildHashString(filePath, hash);
+}
+
+function SHA1Hash(filePath, contents) {
+	var hash = crypto.createHash('sha1').update(contents).digest('hex').slice(0, 10);	
+	return buildHashString(filePath, hash);
+}
+
+function timestampHash(filePath) {
+	var hash = crypto.createHash('md5').update(new Date().toISOString()).digest('hex').slice(0, 10);
+	return buildHashString(filePath, hash);
+}
 
 module.exports = spriteSmash;
