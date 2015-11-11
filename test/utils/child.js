57a2801b5409765031e0786761012b2b21b39284
Alex McAuliffe <alex@masterofmalt.com>
2015-11-11 13:21:47 +0000

    linting for child.js file

--- test/utils/child.js
+++ test/utils/child.js
@@ -1,15 +1,17 @@
+/* global before, describe, it*/
 // Lovingly ripped off from https://github.com/twolfson/gulp.spritesmith/blob/master/test/utils/child.js
 // Load in dependencies
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
 // Define our execution helper
-exports.run = function (cmd) {
-  before(function runFn (done) {
-    exec(cmd, function (err, stdout, stderr) {
-      if (!err && stderr) {
-        err = new Error(stderr);
+exports.run = function(cmd) {
+  before(function runFn(done) {
+    exec(cmd, function(err, stdout, stderr) {
+      let newErr = err;
+      if (!newErr && stderr) {
+        newErr = new Error(stderr);
       }
-      done(err);
+      done(newErr);
     });
   });
 };
