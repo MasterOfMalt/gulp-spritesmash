21f85e0c95848f2cdb7662835a7ef0cf51991edb
Alex McAuliffe <alex@masterofmalt.com>
2015-11-11 12:41:50 +0000

    Lint fix for index.js

--- src/index.js
+++ src/index.js
@@ -1,156 +1,155 @@
-'use strict'
+'use strict';
 
-var through2 = require('through2');
-var crypto = require('crypto');
-var _ = require('lodash');
-var path = require('path');
-var gutil = require('gulp-util');
+const through2 = require('through2');
+const crypto = require('crypto');
+const _ = require('lodash');
+const path = require('path');
+const gutil = require('gulp-util');
 
 function spriteSmash(options) {
-	
-	var opts = options || {};
-	opts.updateFormats = opts.updateFormats || [];
-	opts.revisionFormats = opts.revisionFormats || [];
-	opts.hashFunction = opts.hashFunction || 'MD5';
-	
-	var renames = [];
-	var revisionFormats = [
-		'png',
-		'jpeg',
-		'jpg',
-		'svg',
-		'gif'
-	].concat(opts.revisionFormats)
-	
-	var updateFormats = [
-		'styl',
-		'stylus',
-		'sass',
-		'scss',
-		'less',
-		'css'
-	].concat(opts.updateFormats)
-	
-	var HashFunctions = {
-		'MD5': MD5Hash,
-		'Timestamp': timestampHash,
-		'SHA1': SHA1Hash
-	}
-	
-	var allExtensions = revisionFormats.concat(updateFormats);
-	
-	// Create a stream to take in images
-	var files = [];
-	var onData = function (file, encoding, cb) {
-		if (file.isNull()) {
-			this.push(file);
-			return cb();
-		}
-		
-		if (file.isStream()) {
-			this.emit('error', new gutil.PluginError('gulp-spritesmash', 'Streaming not supported'));
-			return cb();
-		}
-		var skip = false;
-		var fileExt = path.extname(file.path);
-		
-		// Collect renames from reved files.
-		if (file.revOrigPath) {
-			renames.push({
-				originalName: file.revOrigBase,
-				originalPath: path.normalize(path.relative(file.revOrigBase, file.revOrigPath)),
-				newName: file.base,
-				newPath: path.normalize(path.relative(file.base, file.path))
-			});
-			skip = !_.includes(updateFormats, fileExt.slice(1, fileExt.length));
-		}
-		
-		if (!skip && _.includes(allExtensions, fileExt.slice(1, fileExt.length))) {
-			files.push(file);
-		} else {
-			this.push(file);
-		}
-		
-		cb();
-	};
-	
-	var onEnd = function (cb) {
-		var that = this;
-		
-		var revisionFiles = _.filter(files, function(file) {
-			var ext = path.extname(file.path);
-			return _.includes(revisionFormats, ext.slice(1, ext.length));
-		})
-		
-		var updateFiles = _.filter(files, function(file) {
-			var ext = path.extname(file.path);
-			return _.includes(updateFormats, ext.slice(1, ext.length));
-		})
-		
-		revisionFiles.forEach(function(file) {			
-			var hashFunc = _.isFunction(opts.hashFunction) ? 
-							opts.hashFunction :
-							HashFunctions[opts.hashFunction];
-			
-			var filePath = path.parse(file.path)
-			var newName = hashFunc(filePath, file.contents);
-			var originalName = filePath.base;
-			filePath.base = newName;
-			var newPath = path.normalize(path.format(filePath)).split(/[?#]/)[0];
-			var originalPath = file.path.split(/[?#]/)[0];
-						
-			renames.push({
-				newName: newName,
-				newPath: newPath,
-				originalName: originalName,
-				originalPath: originalPath	
-			})
-			
-			file = _.merge(file, {
-				path: newPath,
-				originalName: originalName
-			});
-			
-			that.push(file);
-		}, this);
-		
-		updateFiles.forEach(function(file) {
-			var contents = file.contents.toString();
-			renames.forEach(function replace(renamed) {
-				contents = contents
-							.split(renamed.originalPath)
-							.join(renamed.newPath)
-							.split(renamed.originalName)
-							.join(renamed.newName);
-			});
-			file.contents = new Buffer(contents);
-			that.push(file);
-		}, this);
-		
-		cb();
-	}
-	
-	var retStream = through2.obj(onData, onEnd);
-	return retStream;
-}
+  const renames = [];
 
-function buildHashString(filePath, hash) {
-	return `${filePath.name}-${hash}${filePath.ext}`	
-}
+  const opts = options || {};
+  opts.updateFormats = opts.updateFormats || [];
+  opts.revisionFormats = opts.revisionFormats || [];
+  opts.hashFunction = opts.hashFunction || 'MD5';
 
-function MD5Hash(filePath, contents) {
-	var hash = crypto.createHash('md5').update(contents).digest('hex').slice(0, 10);
-	return buildHashString(filePath, hash);
-}
+  const revisionFormats = [
+    'png',
+    'jpeg',
+    'jpg',
+    'svg',
+    'gif',
+  ].concat(opts.revisionFormats);
 
-function SHA1Hash(filePath, contents) {
-	var hash = crypto.createHash('sha1').update(contents).digest('hex').slice(0, 10);	
-	return buildHashString(filePath, hash);
-}
+  const updateFormats = [
+    'styl',
+    'stylus',
+    'sass',
+    'scss',
+    'less',
+    'css',
+  ].concat(opts.updateFormats);
 
-function timestampHash(filePath) {
-	var hash = crypto.createHash('md5').update(new Date().toISOString()).digest('hex').slice(0, 10);
-	return buildHashString(filePath, hash);
+  function buildHashString(filePath, hash) {
+    return `${filePath.name}-${hash}${filePath.ext}`;
+  }
+
+  function MD5Hash(filePath, contents) {
+    const hash = crypto.createHash('md5').update(contents).digest('hex').slice(0, 10);
+    return buildHashString(filePath, hash);
+  }
+
+  function SHA1Hash(filePath, contents) {
+    const hash = crypto.createHash('sha1').update(contents).digest('hex').slice(0, 10);
+    return buildHashString(filePath, hash);
+  }
+
+  function timestampHash(filePath) {
+    const hash = crypto.createHash('md5').update(new Date().toISOString()).digest('hex').slice(0, 10);
+    return buildHashString(filePath, hash);
+  }
+
+  const HashFunctions = {
+    'MD5': MD5Hash,
+    'Timestamp': timestampHash,
+    'SHA1': SHA1Hash,
+  };
+
+  const allExtensions = revisionFormats.concat(updateFormats);
+
+  // Create a stream to take in images
+  const files = [];
+  const onData = function onData(file, encoding, cb) {
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-spritesmash', 'Streaming not supported'));
+      return cb();
+    }
+    let skip = false;
+    const fileExt = path.extname(file.path);
+
+    // Collect renames from reved files.
+    if (file.revOrigPath) {
+      renames.push({
+        originalName: file.revOrigBase,
+        originalPath: path.normalize(path.relative(file.revOrigBase, file.revOrigPath)),
+        newName: file.base,
+        newPath: path.normalize(path.relative(file.base, file.path)),
+      });
+      skip = !_.includes(updateFormats, fileExt.slice(1, fileExt.length));
+    }
+
+    if (!skip && _.includes(allExtensions, fileExt.slice(1, fileExt.length))) {
+      files.push(file);
+    } else {
+      this.push(file);
+    }
+
+    cb();
+  };
+
+  const onEnd = function onEnd(cb) {
+    const that = this;
+
+    const revisionFiles = _.filter(files, function find(file) {
+      const ext = path.extname(file.path);
+      return _.includes(revisionFormats, ext.slice(1, ext.length));
+    });
+
+    const updateFiles = _.filter(files, function find(file) {
+      const ext = path.extname(file.path);
+      return _.includes(updateFormats, ext.slice(1, ext.length));
+    });
+
+    revisionFiles.forEach(function hashFiles(file) {
+      const hashFunc = _.isFunction(opts.hashFunction) ?
+              opts.hashFunction :
+              HashFunctions[opts.hashFunction];
+
+      const filePath = path.parse(file.path);
+      const newName = hashFunc(filePath, file.contents);
+      const originalName = filePath.base;
+      filePath.base = newName;
+      const newPath = path.normalize(path.format(filePath)).split(/[?#]/)[0];
+      const originalPath = file.path.split(/[?#]/)[0];
+
+      renames.push({
+        newName: newName,
+        newPath: newPath,
+        originalName: originalName,
+        originalPath: originalPath,
+      });
+
+      const newFile = _.merge(file, {
+        path: newPath,
+        originalName: originalName,
+      });
+
+      that.push(newFile);
+    }, this);
+
+    updateFiles.forEach(function updateFileContent(file) {
+      let contents = file.contents.toString();
+      renames.forEach(function replace(renamed) {
+        contents = contents
+              .split(renamed.originalPath)
+              .join(renamed.newPath)
+              .split(renamed.originalName)
+              .join(renamed.newName);
+      });
+      file.contents = new Buffer(contents);
+      that.push(file);
+    }, this);
+
+    cb();
+  };
+
+  return through2.obj(onData, onEnd);
 }
 
 module.exports = spriteSmash;
